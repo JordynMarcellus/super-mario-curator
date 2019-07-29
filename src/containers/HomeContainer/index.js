b5a529c6bb211d6a43a66c3c6e6dd76157a074de
Jordyn Marcellus <jordynmarcellus@gmail.com>
2019-07-29 15:07:05 -0400

    Basic CR app. Let's get tests in there before we expand further

--- src/containers/HomeContainer/index.js
+++ src/containers/HomeContainer/index.js
@@ -1,10 +1,8 @@
 import React, {memo} from 'react'
 import {SearchContainer} from '../SearchContainer'
-import {CourseForm} from '../../components/CourseForm'
 
 export const HomeContainer = memo((props) => (
     <div> 
         <SearchContainer />
-        <CourseForm />
     </div>
 ))
--- src/containers/SearchContainer/index.js
+++ src/containers/SearchContainer/index.js
@@ -1,20 +1,24 @@
 import React, {useContext, useEffect, useState} from "react"
-import {Heading} from 'grommet'
+import {Box, Button, Collapsible, Heading} from 'grommet'
+import {CourseForm} from '../../components/CourseForm'
 import {FirebaseContext} from '../../components/Firebase'
 
 export const SearchContainer = (props) => {
     const {firestoreDB} = useContext(FirebaseContext)
     const [courses, setCourses] = useState([])
+    const [isAddingNewCourse, setIsAddingNewCourse] = useState(false)
+
     useEffect(() => {
         firestoreDB.collection("courses").get().then(snapshot => {
             const newData = snapshot.docs.map(doc => doc.data())
             setCourses(newData)
         }).catch(e => console.error(e))
-    }, [firestoreDB])
+    }, [])
 
     return (
         <>
             <Heading level="1">Random courses:</Heading>
+
             {courses.map(({courseName, makerId, courseId}) => (
                <div key={courseId}>
                    <ul>
@@ -24,6 +28,13 @@ export const SearchContainer = (props) => {
                     </ul>
                 </div>
             ))}
+            
+            <Button label="Add new course" onClick={() => setIsAddingNewCourse(!isAddingNewCourse)} />
+            <Collapsible open={isAddingNewCourse}>
+                <Box>
+                    <CourseForm />
+                </Box>
+            </Collapsible>
         </>
     )
 }
