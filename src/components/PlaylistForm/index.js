cf7239e6fe9df39bb3b64ceaba73bf45752f298d
Jordyn Marcellus <jordynmarcellus@gmail.com>
2019-08-21 14:40:57 -0400

    WIP commit with PlaylistForm taking shape

--- src/components/PlaylistForm/index.js
+++ src/components/PlaylistForm/index.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Box, Button, Form } from "grommet";
+import { Box, Button, Form, TextArea } from "grommet";
 import { FormAdd } from "grommet-icons";
 import { FormTextInput } from "../FormTextInput";
 
@@ -10,13 +10,40 @@ const DEFAULT_PLAYLIST_ADD_STATE = {
 
 export const PlaylistForm = props => {
   const [courses, setCourses] = useState([DEFAULT_PLAYLIST_ADD_STATE]);
+  const [playlistData, setPlaylistData] = useState({
+    name: "",
+    description: "",
+  });
   return (
     <Box>
       <Form onSubmit={e => e.preventDefault()}>
+        <FormTextInput
+          label="Playlist name"
+          id="playlist-name"
+          value={playlistData.name}
+          onChange={e =>
+            setPlaylistData({ ...playlistData, name: e.target.value })
+          }
+          required
+        />
+        <TextArea
+          placeholder="Add a descriptional (optional)"
+          value={playlistData.description}
+          onChange={e =>
+            setPlaylistData({ ...playlistData, description: e.target.value })
+          }
+        />
+        <hr />
+        <Button
+          icon={<FormAdd />}
+          plain
+          label="Add another course"
+          onClick={e => setCourses([...courses, DEFAULT_PLAYLIST_ADD_STATE])}
+        />
         {courses.map(({ name, id }, index) => (
-          <>
+          <Box key={`playlist-course--${index}`}>
             <FormTextInput
-              label="CourseID (add dsahes)"
+              label="CourseID (add dashes)"
               id="courseId-input"
               value={id}
               onChange={e => console.log(e.target.value)}
@@ -30,14 +57,8 @@ export const PlaylistForm = props => {
               onChange={e => console.log(e.target.value)}
               required
             />
-          </>
+          </Box>
         ))}
-        <Button
-          icon={<FormAdd />}
-          reverse
-          label="Add new course"
-          onClick={e => setCourses([...courses, DEFAULT_PLAYLIST_ADD_STATE])}
-        />
       </Form>
     </Box>
   );
