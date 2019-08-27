749843c67d26295e290a376a58bdf2b353a29fa8
Jordyn Marcellus <jordynmarcellus@gmail.com>
2019-08-27 16:40:30 -0400

    Wires PlaylistSingleContainer into firebase

--- src/components/PlaylistCard/index.js
+++ src/components/PlaylistCard/index.js
@@ -1,6 +1,6 @@
 import React from "react";
-import { Box, Button, Heading, Paragraph, Text } from "grommet";
-import { Link } from "react-router-dom";
+import { Box, Heading, Paragraph, Text } from "grommet";
+import { StyledLink } from "../../styles/styles";
 
 export const PlaylistCard = props => (
   <Box
@@ -12,8 +12,14 @@ export const PlaylistCard = props => (
     <Heading level="2" margin={{ vertical: "small" }}>
       {props.playlist.playlistData.playlistName}
     </Heading>
-    <Paragraph>{props.playlist.playlistData.playlistDescription}</Paragraph>
-    <Text>Total courses: {props.playlist.courses.length}</Text>
-    <Link to={`/playlists/${props.playlist.uid}`}>See playlist</Link>
+    <Paragraph margin={{ top: "0", bottom: "small" }}>
+      {props.playlist.playlistData.playlistDescription}
+    </Paragraph>
+    <Text margin={{ bottom: "small" }}>
+      Total courses: {props.playlist.courses.length}
+    </Text>
+    <StyledLink to={`/playlists/${props.playlist.uid}`}>
+      See playlist
+    </StyledLink>
   </Box>
 );
--- src/containers/PlaylistSingleContainer/index.js
+++ src/containers/PlaylistSingleContainer/index.js
@@ -1,3 +1,34 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
+import { Box, Heading, Paragraph } from "grommet";
+import { FirebaseContext } from "../../components/Firebase";
+export const PlaylistSingleContainer = props => {
+  const { firestoreDB } = useContext(FirebaseContext);
+  const [isLoading, setLoadingState] = useState(true);
+  const [playlistInfo, setPlaylistInfo] = useState(null);
+  const { playlistId } = props.match.params;
 
-export const PlaylistSingleContainer = props => <div>👋🏻</div>;
+  useEffect(() => {
+    firestoreDB
+      .collection("playlists")
+      .doc(playlistId)
+      .get()
+      .then(docSnap => {
+        const playlistData = docSnap.exists ? docSnap.data() : null;
+        console.log(playlistData);
+        setPlaylistInfo(playlistData);
+        setLoadingState(false);
+      })
+      .catch(e => console.error(e));
+  }, [playlistId, firestoreDB]);
+  return (
+    <Box>
+      {isLoading && <div>Loading...</div>}
+      {playlistInfo && (
+        <>
+          <Heading level="2">{playlistInfo.playlistData.playlistName}</Heading>
+          <Paragraph>{playlistInfo.playlistData.playlistDescription}</Paragraph>
+        </>
+      )}
+    </Box>
+  );
+};
