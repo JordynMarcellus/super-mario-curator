df1a3639f49d6594a48fb3505ea40ec89b2192a6
Jordyn Marcellus <jordynmarcellus@gmail.com>
2021-11-25 06:44:51 -0500

    Basic page shell for ctm

---
+++ pages/crash-the-market/index.js
@@ -0,0 +1,14 @@
+import { Layout } from "../../components/Layout";
+import { CustomHead } from "../../components/CustomHead";
+
+const Main = () => (
+  <Layout>
+    <CustomHead
+      title="It's time to crash Toronto's housing market"
+      seoPreviewTitle="Crash The Market"
+      seoPreviewContent="The only way to stop housing exploitation is to take the money out of it."
+    />
+  </Layout>
+);
+
+export default Main;
