2059a31f3bd787d32b5b4eb00bd709b4956e249e
Jordyn Marcellus <jordynmarcellus@gmail.com>
2021-11-25 06:26:17 -0500

    WiP with Toronto based copy

--- pages/i-hate-toronto/index.js
+++ pages/i-hate-toronto/index.js
@@ -6,32 +6,15 @@ import { CustomHead } from "../../components/CustomHead";
 const Main = () => (
   <Layout>
     <CustomHead
-      title="I Hate Toronto. Torontonians taking their city from the ruling class."
+      title="Torontonians taking back the city from the ruling class."
       seoPreviewTitle="I Hate Toronto"
-      seoPreviewContent="Toronto is decadent depraved and we're going to shine a light on it."
+      seoPreviewContent="Toronto is decadent and depraved. Let's shine a light on it."
     />
     <section className="h8-center">
       <h1>Toronto is decadent and depraved.</h1>
-      <p>Feel the same? Let me know.</p>
-      <form name="intros" method="POST" data-netlify="true">
-        <Input type="email" name="email" labelText="Email" required={true} />
-        <Input
-          type="text"
-          name="name"
-          labelText="Name (optional)"
-          required={false}
-        />
-        <TextArea
-          name="message"
-          required={true}
-          labelText={"What do you hate about Toronto?"}
-        />
-        <button type="submit">Submit</button>
-      </form>
-      <p>
-        FYI: we collect your information with Netlify Forms over a secure
-        connection. Be chill, we have no intent to sell it.
-      </p>
+      <p>As the pandemic raged, renovictions surged.</p>
+      <p>Vacancies increase as rents rise.</p>
+      <p>The greedy, tasteless ruling class has won.</p>
     </section>
     <style jsx>
       {`
