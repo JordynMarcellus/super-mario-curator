83e20189881a114cb108309d37a87f047b595f55
Jordyn Marcellus <jordynmarcellus@gmail.com>
2021-11-29 18:46:56 -0500

    Fixes issue with complete cruft

--- components/forms/Input.js
+++ components/forms/Input.js
@@ -1,9 +1,6 @@
-export const CommonLabelStyles = () => ();
-
 export const Input = ({ type, name, required, labelText }) => (
   <label>
     <span>{labelText}</span>
     <input {...{ type, name, required }} required />
-    <CommonLabelStyles />
   </label>
 );
--- components/forms/TextArea.js
+++ components/forms/TextArea.js
@@ -1,9 +1,6 @@
-import { CommonLabelStyles } from "./Input";
-
 export const TextArea = ({ name, required, labelText }) => (
   <label>
     <span>{labelText}</span>
     <textarea {...{ name, required }} required />
-    <CommonLabelStyles />
   </label>
 );
